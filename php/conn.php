34da0f27f283b556406534fdd6b1602668a1c402
Slinkter <luis.j.cueva@gmail.com>
2019-07-03 16:21:09 -0500

    dbContactsArsi

--- php/conn.php
+++ php/conn.php
@@ -1,9 +1,22 @@
 <?php
-$db_name = "dbUserArsi";
-$usernama = "userArsi";
-$password = "MUNDOarsi20    ";
-$servername = "11.78.0.27";
-$conn=mysqli_connect();
-mysqli_set_charset($conn,"utf-8");
+$user_name = "userArsi";
+$password = "MUNDOarsi20";
+$server = "11.78.0.27"
+$db_name = "dbContactsArsi";
+
+$con = mysql_connect($server,$user_name,$password,$db_name);
+if(con){
+    $Name = $_POST['name']
+    $query = "insert into contacts(name) values ('"$Name"')";
+    $result = mysqli_query($con,$query);
+    $response = array();
+    if($result){
+        $status = 'OK';
+
+    }else {
+        $status = 'FAILED';
+    }
+}
+
 
 ?>
