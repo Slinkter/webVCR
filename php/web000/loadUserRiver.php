97c0c26d9d7f6c509b7398c7f789e93c440ed4dc
Slinkter <luis.j.cueva@gmail.com>
2019-07-18 15:26:05 -0500

    funciona

--- php/web000/loadUserRiver.php
+++ php/web000/loadUserRiver.php
@@ -2,9 +2,9 @@
 
 require "connect.php";
 
-$uid = $_GET["uid"];
+$uid = AVzQ7F13iXYHiTJs8rDsbLc3fqt1;
 
-$query = "SELECT * FROM login_river; ";
+$query = "SELECT * FROM login_river where uid = '$uid'; ";
 
 if(mysqli_query($conn,$query)){
 
@@ -17,10 +17,18 @@ if(mysqli_query($conn,$query)){
   
     $query_run = mysqli_query($conn,$query);
     while($row = mysqli_fetch_array($query_run)){
+
+        $response[$firstname] = $row[1];
+        $response[$lastname]= $row[2];
+        $response[$dni] = $row[3];
+        $response[$correo]= $row[4];
+        $response[$numphone]= $row[5];
         echo $row[0].' '.$row[1].' '.$row[2].' '.$row[3].' '.$row[4].' '.$row[5].' '.$row[6].' '.$row[7].' ' ;
         echo '<br/>';    
     }  
-    
+   
+
+
     $response['success'] = true;
     $response['message']= "Successfuly";
 }else {
