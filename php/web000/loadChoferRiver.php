8b24b4bcc89f22f4e2bf16e0d0f5a76cf914bcf9
Slinkter <luis.j.cueva@gmail.com>
2019-07-20 11:55:09 -0500

    usando json

---
+++ php/web000/loadChoferRiver.php
@@ -0,0 +1,41 @@
+<?php
+
+require "connect.php";
+
+$query = "SELECT * FROM choferRiver ; ";
+
+if(mysqli_query($conn,$query)){
+
+    $id = 'id';
+    $firstname = 'nameChofer';
+    $lastname = 'lastChofer';
+    $dni = 'dniChofer';
+    $correo = 'brevete';
+    $numphone = 'numphone'; 
+    
+  
+    $query_run = mysqli_query($conn,$query);
+    while($row = mysqli_fetch_array($query_run)){
+
+        $response[$firstname] = $row[1];
+        $response[$lastname]= $row[2];
+        $response[$dni] = $row[3];
+        $response[$correo]= $row[4];
+        $response[$numphone]= $row[5];
+        $List[]=$response;
+         
+    }  
+     
+    $response['success'] = true;
+    $response['message']= "Successfuly";
+}else {
+    $response['success'] = false;
+    $response['message']= "Failure!";
+ }
+
+
+//echo json_encode($response);
+echo json_encode($List);
+mysql_close($conn);
+?>
+ 
