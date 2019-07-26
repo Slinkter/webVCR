48ac23f55005d183071acf8c99d7a14cab9c302a
Slinkter <luis.j.cueva@gmail.com>
2019-07-26 12:38:52 -0500

    ok

--- php/web000/loadChoferRiver.php
+++ php/web000/loadChoferRiver.php
@@ -12,8 +12,6 @@ if(mysqli_query($conn,$query)){
     $dni = 'dniChofer';
     $correo = 'brevete';
     $numphone = 'numphone'; 
-    
-    $json = array();
 
     $query_run = mysqli_query($conn,$query);
     while($row = mysqli_fetch_array($query_run)){
@@ -23,24 +21,18 @@ if(mysqli_query($conn,$query)){
         $response[$dni] = $row[3];
         $response[$correo]= $row[4];
         $response[$numphone]= $row[5];
-
         $List[]=$response;
-        $json['ListaChofer'][] = $row; 
+        
     }  
      
     $json['success'] = true;
     $json['message']= "Successfuly";
-  
-
 }else {
     $response['success'] = false;
     $response['message']= "Failure!";
  }
 
-
-//echo json_encode($response);
 echo json_encode($List);
-//echo json_encode($json);
 mysql_close($conn);
 ?>
  
