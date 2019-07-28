6648ce2a2e176e751cc810dbc48696bace420422
Slinkter <luis.j.cueva@gmail.com>
2019-07-28 15:43:17 -0500

    actualizar

---
+++ php/web000/saveVehiculoRiver.php
@@ -0,0 +1,23 @@
+<?php
+
+require "connect.php";
+
+$firstname = $_GET["nameChofer"];
+$lastname = $_GET["lastChofer"];
+$dni = $_GET["dniChofer"];
+$brevete = $_GET["brevete"];
+$numphone = $_GET["numphone"];
+
+$query = "INSERT INTO `vehiculoRiver`(`nameChofer`,`lastChofer`,`dniChofer`,`brevete`,`numphone`) VALUES ('$firstname','$lastname','$dni','$brevete','$numphone');";
+
+if(mysqli_query($conn,$query)){
+        $response['success'] = true;
+        $response['message']= "Successfuly";
+}else {
+        $response['success'] = false;
+        $response['message']= "Failure!";
+ }
+echo json_encode($response);
+mysql_close($conn);
+?>
+ 
